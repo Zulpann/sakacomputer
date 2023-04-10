c4b6149dc754321dce245957f79fb10d5530a38d
Dzulfan Nur Fachrudin <90804983+Zulpann@users.noreply.github.com>
2023-04-10 09:26:56 +0700

    add fitur

---
+++ script.js
@@ -0,0 +1,39 @@
+const bigBtn = document.getElementsByClassName("big-button")[0];
+bigBtn.addEventListener('click', function(){
+    alert("Di Click")
+})
+
+// const liNavigationProduct = document.querySelector('.navigation-product ul li');
+// liNavigationProduct.style.color = "red";
+
+const wraperCardGaming = document.querySelector('.wrap-gaming');
+wraperCardGaming.style.display = "none"
+const wraperCardProvesional = document.querySelector('.wrap-profesional');
+wraperCardProvesional.style.display = "none"
+const wraperCardMultimedia = document.querySelector('.wrap-multimedia');
+wraperCardMultimedia.style.display = "none"
+
+const gaming = document.getElementById('gaming')
+gaming.addEventListener('click', function(){
+    wraperCardGaming.style.display = "flex"
+    if(true){
+        wraperCardProvesional.style.display = "none"
+        wraperCardMultimedia.style.display = "none"
+    }
+});
+const profesional = document.getElementById('personal')
+profesional.addEventListener('click', function(){
+    wraperCardProvesional.style.display = "flex"
+    if(true){
+        wraperCardMultimedia.style.display = "none"
+        wraperCardGaming.style.display = "none"
+    }
+});
+const multimedia = document.getElementById('multimedia')
+multimedia.addEventListener('click', function(){
+    wraperCardMultimedia.style.display = "flex"
+    if(true){
+        wraperCardGaming.style.display = "none"
+        wraperCardProvesional.style.display = "none"
+    }
+});
